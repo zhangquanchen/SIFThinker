a9de5321e093d79a688f961e3c9a216bced66fa5
Zhangquan Chen <czq23@mails.tsinghua.edu.cn>
2025-11-13 11:59:35 +0800

    update

--- GRPO-SIF/src/open-r1-multimodal/src/open_r1/vlm_modules/__init__.py
+++
@@ -1,5 +0,0 @@
-from .vlm_module import VLMBaseModule
-from .qwen_module import Qwen2VLModule
-from .internvl_module import InvernVLModule
-
-__all__ = ["VLMBaseModule", "Qwen2VLModule", "InvernVLModule"]
